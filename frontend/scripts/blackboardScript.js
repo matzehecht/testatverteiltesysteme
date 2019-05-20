c06cab63a4cf442cfe53769f1044562bf4dc259b
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2019-05-20 18:27:06 +0200

    added jQuery implemented getBlackboards()

--- frontend/scripts/blackboardScript.js
+++ frontend/scripts/blackboardScript.js
@@ -1,27 +1,40 @@
-function createBlackboard(name) {
+$(document).ready(function(){
+    var apiUrl = "http://localhost:8080/api"
 
-}
+	function createBlackboard(name) {
 
-function updateBlackboard(name, message) {
-	
-}
+	}
 
-function clearBlackboard(name) {
-	updateBlackboard(name, "");
-}
+	function updateBlackboard(name, message) {
+		
+	}
 
-function readBlackboard(name) {
+	function clearBlackboard(name) {
+		updateBlackboard(name, "");
+	}
 
-}
+	function readBlackboard(name) {
 
-function getBlackboardStatus(name) {
-	
-}
+	}
 
-function getBlackboards(handler) {
-	
-}
+	function getBlackboardStatus(name) {
+		
+	}
 
-function deleteBlackboard(name) {
-	
-}
+	function getBlackboards(handler, errorHandler) {
+		$.ajax({
+			url: apiUrl + '/blackboard',
+			data: {
+				format: 'json'
+			},
+			error: errorHandler(),
+			dataType: 'jsonp',
+			success: handler(data),
+			type: 'GET'
+		});
+	}
+
+	function deleteBlackboard(name) {
+		
+	}
+});
